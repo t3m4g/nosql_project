1b30a9bcd7201ce177d77c76aa663100f94bae7f
Mawulé Gérard Kodjo Patient TOUDOGUIN <gerard.toudoguin@uac.bj>
2025-06-26 09:35:05 +0100

    c'est le nuveau code de mon devoir de nosql. faut absolument que j'assure

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,35 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import Layout from "./components/Layout";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-import { Toaster } from "./components/Toast/toaster";
-import { Toaster as Sonner } from "./components/Toast/sonner";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-
-
-
-function AppRoutes() {
-  return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <Layout>
-            <Index />
-          </Layout>
-        }
-      />
-      <Route
-        path="/login"
-        element={
-          <Layout>
-            <Login />
-          </Layout>
-        }
-      />
-      <Route
-        path="/register"
-        element={
-          <Layout>
-            <Register />
-          </Layout>
-        }
-      />
-      <Route
-        path="*"
-        element={
-          <Layout>
-            <NotFound />
-          </Layout>
-        }
-      />
-    </Routes>
-  );
-}
-
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <React.StrictMode>
-      <BrowserRouter future={{ v7_startTransition: true }}>
-        <Toaster />
-        <Sonner />
-        <AppRoutes />
-      </BrowserRouter>
-    </React.StrictMode>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
